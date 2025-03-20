c7dc79531eacaf2c958ebdb2d94c4f83241c8685
kalavhan <josue.brigido1@gmail.com>
2025-03-20 12:43:32 -0600

    [feat/Navbar]: Use scss instead of css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import Footer from "./components/Footer";
-import "./styles/global.css";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,13 +14,9 @@ function App() {
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
-        <Footer />
       </div>
     </Router>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/global.scss";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4 flex justify-between">
+    <nav className="navbar">
       <Link to="/" className="text-xl font-bold">Portfolio</Link>
       <div className="space-x-4">
         <Link to="/about">About</Link>
